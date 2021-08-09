2f0ec032f0aaadb4a8ac42ad9d45e3135f8d027c
BudacaB <budaca.b@gmail.com>
2021-08-09 10:56:36 +0300

    started resource server

---
+++ demo-springcloud-m4-secureservice/src/test/java/demospringcloudm4secureservice/demospringcloudm4secureservice/DemoSpringcloudM4SecureserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package demospringcloudm4secureservice.demospringcloudm4secureservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudM4SecureserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- demo-springcloud-m4-secureui/src/main/java/demospringcloudm4secureui/ReportController.java
+++ demo-springcloud-m4-secureui/src/main/java/demospringcloudm4secureui/ReportController.java
@@ -1,16 +1,29 @@
 package demospringcloudm4secureui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ReportController extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private OAuth2ClientContext clientContext; // deprecated
+
     @RequestMapping("/")
     public String loadHome() {
         return "home";
+    }
+
+    public String loadReports(Model model) {
+        OAuth2AccessToken t = clientContext.getAccessToken();
+        System.out.println("Token: " + t.getValue());
+
+        return "reports";
     }
 
     @Override
